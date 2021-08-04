cfe3ad9ce8ea62db0a027add761f0d8e3408cd54
Andrew Jones <ajones@digitalbazaar.com>
2021-08-04 17:44:49 -0400

    Add correct context url.

--- js/constants.js
+++ js/constants.js
@@ -5,7 +5,7 @@
 
 module.exports = {
   CONTEXT_FILENAME: 'vdl-v1.jsonld',
-  CONTEXT_URL: 'https://w3id.org/vdl',
+  CONTEXT_URL: 'https://w3id.org/vdl/v1',
   // value between 0x0 and 0x7FFF for globally registered term codec values
   // or >= 0x8000 for app-specific local terms
   CBORLD_VALUE: 0x20
