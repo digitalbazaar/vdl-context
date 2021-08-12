4b088830b62db2d0de403383756be92aa55409d8
Andrew Jones <ajones@digitalbazaar.com>
2021-08-12 10:22:35 -0400

    Add test for signing vcs that use this context.

--- test/loader.js
+++ test/loader.js
@@ -4,10 +4,8 @@
 'use strict';
 
 const didKey = require('@digitalbazaar/did-method-key');
-const citContext = require('cit-context');
 const didContext = require('did-context');
 const ed25519 = require('ed25519-signature-2020-context');
-const x25519 = require('x25519-key-agreement-2020-context');
 const cred = require('credentials-context');
 const {JsonLdDocumentLoader} = require('jsonld-document-loader');
 const {CONTEXT_URL: testContextUri, CONTEXT} = require('../');
@@ -19,12 +17,6 @@ const staticLoader = new JsonLdDocumentLoader();
 staticLoader.addStatic(ed25519.constants.CONTEXT_URL,
   ed25519.contexts.get(ed25519.constants.CONTEXT_URL));
 
-staticLoader.addStatic(x25519.constants.CONTEXT_URL,
-  x25519.contexts.get(x25519.constants.CONTEXT_URL));
-
-staticLoader.addStatic(citContext.constants.CONTEXT_URL,
-  citContext.contexts.get(citContext.constants.CONTEXT_URL));
-
 staticLoader.addStatic(didContext.constants.DID_CONTEXT_URL,
   didContext.contexts.get(didContext.constants.DID_CONTEXT_URL));
 
@@ -35,7 +27,7 @@ staticLoader.addStatic(testContextUri, CONTEXT);
 
 const didKeyDriver = didKey.driver();
 
-export const documentLoader = async url => {
+const documentLoader = async url => {
   if(url && url.startsWith('did:key')) {
     const document = await didKeyDriver.get({url});
     return {
@@ -48,3 +40,5 @@ export const documentLoader = async url => {
 
   return staticLoader.documentLoader(url);
 };
+
+module.exports = documentLoader;
--- test/mocha/vc.spec.js
+++ test/mocha/vc.spec.js
@@ -2,16 +2,52 @@
  * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
  */
 const chai = require('chai');
+const didKeyDriver = require('@digitalbazaar/did-method-key').driver();
+const jsigs = require('jsonld-signatures');
+const documentLoader = require('../loader');
+const {Ed25519Signature2020} = require('@digitalbazaar/ed25519-signature-2020');
+
+const {purposes: {AssertionProofPurpose}} = jsigs;
 chai.should();
-const {expect} = chai;
-
-const {contexts, constants} = require('../..');
-
-const contextUrl = constants.CONTEXT_URL;
 
 describe('Sign with Context', () => {
 
-  it('contexts', async () => {
-    expect(contexts.get(contextUrl)).to.have.property('@context');
+  it('signs a vc that uses this context', async () => {
+    const credential = {
+      '@context': [
+        'https://www.w3.org/2018/credentials/v1',
+        'https://w3id.org/vdl/v1'
+      ],
+      type: [
+        'VerifiableCredential',
+        'Iso18013DriversLicense'
+      ],
+      credentialSubject: {
+        id: 'did:key:z6MkrniA4wQR3eoWvpaTbCpdyjoowsRQYeXx2e3vLUp4jk4e',
+        type: 'Iso18013DriversLicense',
+        license: {
+          document_number: '542426814',
+          family_name: 'TURNER',
+          given_name: 'SUSAN',
+          portrait: '/9j/4AAQSkZJRgABAQEAkACQA...gcdgck5HtRRSClooooP/2Q==',
+          birth_date: '1998-08-28',
+          issue_date: '2018-01-15T10:00:00Z',
+          expiry_date: '2022-08-27T12:00:00Z',
+          issuing_country: 'US',
+          issuing_authority: 'AL',
+          un_distinguishing_sign: 'USA'
+        }
+      }
+    };
+    const {methodFor} = await didKeyDriver.generate();
+    const keyPair = methodFor({purpose: 'verificationMethod'});
+    const suite = new Ed25519Signature2020({key: keyPair});
+    const purpose = new AssertionProofPurpose();
+    const result = await jsigs.sign(credential, {
+      purpose,
+      documentLoader,
+      suite
+    });
+    console.log(result);
   });
 });
